b0abaf9e7fd8ab82f7add9fa09e69a2dac39ae60
Alan Beck <beckalan303@gmail.com>
2025-02-22 13:55:05 -0300

    my first commit is here

---
+++ src/components/LinkComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+defineProps(['to'])
+</script>
+<template>
+  <router-link
+    :to="{ name: to }"
+    class="rounded bg-green-400 hover:bg-green-500 font-bold py-2 px-10"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+<style scoped></style>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LinkView from '@/components/LinkComponent.vue'
+</script>
+<template>
+  <header class="px-10 py-5 bg-gray-700 flex justify-between absolute top-0 w-full z-10">
+    <nav>
+      <LinkView to="admin">Administrador</LinkView>
+    </nav>
+  </header>
+</template>
+<style scoped></style>
